a3f36e859237efe034f088090a52e3564c72c300
FixbroYT <135733422+FixbroYT@users.noreply.github.com>
2025-02-18 16:43:33 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
-TOKEN = ''
+import os
+from aiogram import Bot
+
+TOKEN = os.getenv("BOT_TOKEN")  # Получаем токен из переменных окружения
+
+if not TOKEN:
+    raise ValueError("Bot token is missing! Set BOT_TOKEN in environment variables.")
+
+bot = Bot(token=TOKEN)
