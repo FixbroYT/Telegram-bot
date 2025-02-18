fa35204129606a2bdf897fef357c8fe94dbc9e17
FixbroYT <135733422+FixbroYT@users.noreply.github.com>
2025-02-18 22:13:42 +0100

    Update config.py

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ TOKEN = os.getenv("BOT_TOKEN")
 if not TOKEN:
     raise ValueError("Bot token is missing! Set BOT_TOKEN in environment variables.")
 
-print(f"Твой токен: {TOKEN}")
+print(f"Your token: {TOKEN}")
